a112842b5d19c3e8a473f619c8db8ec54eaf3473
Vivek <vivek9142@gmail.com>
2022-05-29 16:11:11 +0530

    added auth in Navigation component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navigation = (props) => {
+  const {isAuthenticated,loginWithRedirect,logout} = useAuth0();
   return (
     <div className="header">
-      <ul className="nav nav-pills pull-right">
-          <li><Link to="/">Home</Link></li>
+      {!isAuthenticated && (
+        <ul className="nav nav-pills pull-right">
+          <li><button onClick={()=>loginWithRedirect({})}>Log in</button></li>
+      </ul>
+      )}
+
+      {isAuthenticated && (
+        <ul className="nav nav-pills pull-right">
+        <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><button onClick={()=>logout()}>Logout</button></li>
       </ul>
+      )}
+
       <h3 className="text-muted">Securing React</h3>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
